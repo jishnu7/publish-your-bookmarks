b7618649b8f53dafc955dd3c90b8f0ffcf8f4268
Jishnu <jishnu7@gmail.com>
2012-09-15 16:36:08 +0530

    add favicon and blank css

--- config.php
+++ config.php
@@ -6,3 +6,6 @@ $FILE_NAME = "bookmarks.json";
 # Exclude following Folders or Title.
 $EXCLUDE = array("Bookmarks Menu", "Tags");
 
+# Show Favicon True/False
+$FAVICON = True;
+
--- read.php
+++ read.php
@@ -1,25 +1,30 @@
 <?php
 
-function read( $node, $data = '' ) {
+function read($node, $data = '') {
 
     global $EXCLUDE;
     global $FILE_NAME;
+    global $FAVICON;
 
-    if( gettype($node) == 'object' || isset($node->type) ){
+    if(gettype($node) == 'object' || isset($node->type)) {
         if (in_array($node->title, $EXCLUDE)) {
             return $data;
         }
-        if(isset($node->children)){
+        if(isset($node->children)) {
             if ($node->title != NULL) {
                 $data .= ("<li><h2>" . $node->title. "</h2></li>");
             }
             $data .= '<ul>';
-            foreach($node->children as $key => $child){
+            foreach($node->children as $key => $child) {
                     $data = read($child, $data );
             }
             $data .= '</ul>';
         }else{
-                $data .= convert( $node->title, @$node->uri );
+            if ($FAVICON) {
+                $data .= convert($node->title, @$node->uri, get_favicon(@$node->uri));
+            } else {
+                $data .= convert($node->title, @$node->uri, False);
+            }
         }
     }else{
         $data .= "Seems like ". $FILE_NAME ." is not a json bookmark file";
@@ -28,13 +33,24 @@ function read( $node, $data = '' ) {
 }
 
 
-function convert( $title, $url ){
-        if($url){
-                $link = '<li><a href="' . $url . '" target="_blank">' . $title . '</a></li>';
+function convert($title, $url, $favicon) {
+        if($url) {
+            $link = '<li>';
+            if ($favicon) {
+                $link .= '<img src="'. $favicon .'" />';
+            }
+            $link .= '<a href="' . $url . '" target="_blank">' . $title . '</a></li>';
         }else{
-                $link = '<li><a href="#" style="color:#ccc">' . $title . '</a></li>';
+            $link = '<li><a href="#" style="color:#ccc">' . $title . '</a></li>';
         }
         return $link;
 }
 
-
+function get_favicon($url) {
+    $pieces = parse_url($url);
+    $domain = isset($pieces['host']) ? $pieces['host'] : '';
+    if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs)) {
+        return 'http://' . $regs['domain'] . '/favicon.ico';
+    }
+    return False;
+}
